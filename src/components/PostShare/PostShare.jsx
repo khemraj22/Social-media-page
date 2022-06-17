6cc8861acbe533637042dbaaed89b6a3faa9300b
khemraj22 <khemraj.cadd@gmail.com>
2022-06-17 20:34:56 +0530

    image upload functionality

--- src/components/PostShare/PostShare.jsx
+++ src/components/PostShare/PostShare.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import ProfileImage from '../../img/profileImg.jpg'
 import './PostShare.css'
-import { UilScenery, UilPlayCircle, UilLocationPoint, UilSchedule } from '@iconscout/react-unicons';
+import { UilScenery, UilPlayCircle, UilLocationPoint, UilSchedule, UilTimes } from '@iconscout/react-unicons';
 
 const PostShare = () => {
+
+    const [image, setImage] = useState(null)
+    const imageRef = useRef()
+    const onImageChange = (event) => {
+        if (event.target.files && event.target.files[0]) {
+            let img = event.target.files[0]
+            setImage({ image: URL.createObjectURL(img) })
+        }
+    }
     return (
         <div className="PostShare">
             <img src={ProfileImage} alt='Profile' />
@@ -12,7 +21,8 @@ const PostShare = () => {
 
                 <div className="postOptions">
                     <div className="option"
-                        style={{ color: "var(--photo)" }}>
+                        style={{ color: "var(--photo)" }}
+                        onClick={() => imageRef.current.click()}>
                         <UilScenery />
                         Photo
                     </div>
@@ -32,8 +42,16 @@ const PostShare = () => {
                         Schedule
                     </div>
                     <button className='button ps-share'>Share</button>
-
+                    <div style={{ display: 'none' }}>
+                        <input type='file' name='myImage' ref={imageRef} onChange={onImageChange} />
+                    </div>
                 </div>
+                {image &&
+                    <div className="previewImage">
+                        <UilTimes onClick={() => setImage(null)} />
+                        <img src={image.image} alt="" />
+                    </div>
+                }
             </div>
         </div>
 
