84ceaab9975169ea0e3d3ee0095e0855ffda30d1
khemraj22 <khemraj.cadd@gmail.com>
2022-06-18 10:45:05 +0530

    post section done

---
+++ src/data/PostsData.js
@@ -0,0 +1,25 @@
+import postPic1 from '../img/postpic1.jpg'
+import postPic2 from '../img/postpic2.jpg'
+import postPic3 from '../img/postpic3.jpg'
+
+export const PostsData = [{
+    img: postPic1,
+    name: 'Tzuyu',
+    desc: 'Happy New year',
+    likes: 230,
+    liked: true
+},
+{
+    img: postPic2,
+    name: 'Maryam',
+    desc: 'Party Time',
+    likes: 3440,
+    liked: false
+},
+{
+    img: postPic3,
+    name: 'Slena Gomez',
+    desc: 'An archery festival',
+    likes: 80,
+    liked: false
+}]
